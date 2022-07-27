a34266b7bff5767a7d857be4f6c8bbe4f591db88
alkihuri <alkihuri@gmail.com>
2022-07-27 08:50:18 +0300

    ReadyOne

---
+++ VRQuest.Unity/Assets/Scripts/Graber.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graber : MonoBehaviour
+{
+    RaycastHit objectOnHitLine;
+    // Update is called once per frame
+    void Update()
+    { 
+        if(Physics.Raycast(transform.position,transform.forward,out objectOnHitLine))
+        {
+            if(objectOnHitLine.transform.gameObject.tag == "Grab")
+            {
+                objectOnHitLine.transform.Rotate(0, 1, 1);
+            }
+        }
+    }
+}
